f0dab7c7cd316efa044f7dc59f3a567968c4328e
sascha <sastrand@pdx.edu>
2018-11-08 09:57:34 -0800

    built out import exercises

--- main.py
+++ main.py
@@ -1,41 +1,70 @@
-# [add an import statment here]
+# import the xlrd package
 import xlrd
+
 
 def open_fish_file():
     """
     Opens `pdx_fish.xls` and returns contents as column-major list of
-    lists.
+    lists. Dates are left as floating point values in Excel's epoch format.
     """
     pdx_fish_file = "./pdx_fish_test.xls"
+
+    # open workbook, creating an object of the xlrd `Book` class
+    # recommended xlrd function `open_workbook`
     wb = xlrd.open_workbook(pdx_fish_file)
 
-    # takes the first worksheet in the workbook
+    # assign the first worksheet in the workbook to the name `pdx_fish`
+    # recommended xlrd `Book` method: `sheet_by_index()`
+    # pdx_fish = [FILL IN HERE]
     pdx_fish = wb.sheet_by_index(0)
 
-    # sets cursor to row 0, column 0
-    pdx_fish.cell_value(0, 0)
+    # the number of columns in the worksheet
+    # recommended xlrd `Book` attribute : `ncols`
+    # quant_columns = [FILL IN HERE]
+    quant_columns = pdx_fish.ncols
+
+    # the number of rows in the worksheet
+    # recommended xlrd `Book` attribute: `nrows`
+    # quant_rows = [FILL IN HERE]
+    quant_rows = pdx_fish.nrows
 
     # iterates through xlrd worksheet and copies values to list of lists
     worksheet = list()
-    for col in range(pdx_fish.ncols):
+    for col in range(quant_columns):
         worksheet.append(list())
-        for row in range(pdx_fish.nrows):
+        for row in range(quant_rows):
             worksheet[col].append(pdx_fish.cell_value(row, col))
 
     return worksheet
 
 
-def print_worksheet(worksheet):
+def print_worksheet_xlrd(worksheet_xlrd):
+    """
+    Takes a worksheet in the form of an xlrd worksheet object
+    and prints each column to std out.
+    """
+    for col in range(len(worksheet_xlrd)):
+        for row in range(len(worksheet_xlrd[0])):
+            print(worksheet_xlrd[col][row])
+        print("\n")
+
+
+def print_worksheet_lol(worksheet_lol):
     """
     Takes a worksheet in the form of a column-major list of lists
-    and prints it to std out.
+    and prints each column to std out.
     """
-    for col in range(len(worksheet)):
-        for row in range(len(worksheet[0])):
-            print(worksheet[col][row])
+    for col in range(len(worksheet_lol)):
+        for row in range(len(worksheet_lol[0])):
+            print(worksheet_lol[col][row])
         print("\n")
 
 
 if __name__ == "__main__":
-    ws = open_fish_file()
-    print_worksheet(ws)
+    # pass tells the interpreter to do nothing in this block
+    pass
+
+    # From the file `pdx_fish.xls`, what is the average fish length observed
+
+    # From the file `pdx_fish.xls`, what is the largest Reticulate sculpin
+    # obsverved?
