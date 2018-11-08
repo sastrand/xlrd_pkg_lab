693871799a41081269c462ab8d7e5fc6cdf132bd
sascha <sastrand@pdx.edu>
2018-11-08 11:02:10 -0800

    removed example solutions

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # import the xlrd and statistics
-import xlrd
-import statistics
 
 
 def open_fish_file():
@@ -12,22 +10,19 @@ def open_fish_file():
 
     # open workbook, creating an object of the xlrd `Book` class
     # recommended xlrd function `open_workbook`
-    wb = xlrd.open_workbook(pdx_fish_file)
+    # [FILL IN HERE]
 
-    # assign the first worksheet in the workbook to the name `pdx_fish`
-    # recommended xlrd `Book` method: `sheet_by_index()`
+    # assign the first worksheet in the workbook you just created to the
+    # name `pdx_fish` recommended xlrd `Book` method: `sheet_by_index()`
     # pdx_fish = [FILL IN HERE]
-    pdx_fish = wb.sheet_by_index(0)
 
     # the number of columns in the worksheet
     # recommended xlrd `Book` attribute : `ncols`
     # quant_columns = [FILL IN HERE]
-    quant_columns = pdx_fish.ncols
 
     # the number of rows in the worksheet
     # recommended xlrd `Book` attribute: `nrows`
     # quant_rows = [FILL IN HERE]
-    quant_rows = pdx_fish.nrows
 
     # iterates through xlrd worksheet and copies values to list of lists
     worksheet = list()
@@ -88,20 +83,8 @@ if __name__ == "__main__":
     # Using the `statistics` package, find the mean and standard deviation
     # of the lengths of the fish in `pdx_fish.xls`, disregarding all
     # non-numeric values.
-
-    lengths_all = wb[names["fish_length_mm"]]
-    lengths = nums_only_in_list(lengths_all)
-    print("mean fish length: " + str(statistics.mean(lengths)))
-    print("std dev of fish lengths: " + str(statistics.stdev(lengths)))
+    # [FILL IN HERE]
 
     # From the file `pdx_fish.xls`, what is the largest Reticulate Sculpin
     # obsverved? (again, disregarding non-numeric values)
-
-    sculpin_lengths = list()
-
-    for r in range(len(wb[0])):
-        if (wb[names["common_name"]][r] == "Reticulate Sculpin"
-           and isinstance(wb[names["fish_length_mm"]][r], (int, float))):
-            sculpin_lengths.append(wb[names["fish_length_mm"]][r])
-
-    print("largest sculpin: " + str(max(sculpin_lengths)))
+    # [FILL IN HERE]
